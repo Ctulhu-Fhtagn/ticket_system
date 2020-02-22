454f361b9f2d2f51e63d8ed16ae945ecc33ff4c9
Ctulhu-Fhtagn <ctulhu.0.fhtagn@gmail.com>
2020-02-23 00:31:02 +0300

    Add form, all links work

--- ticket_system/ticket/views.py
+++ ticket_system/ticket/views.py
@@ -4,15 +4,34 @@ from django.urls import reverse
 from ticket_system.ticket.models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
+from ticket_system.ticket.forms import AddTaskForm
 
 # Create your views here.
 @login_required(login_url='/admin/login')
 def task_list(request):
     tasks = Task.objects.all()
+    form = AddTaskForm()
     context = {
         'title': 'Tasks',
         'tasks': tasks,
+        'form': form
     }
+    if request.method == 'POST':
+        form = AddTaskForm(request.POST)
+        if form.is_valid():
+            new_task = Task(
+                # client=user,
+                title=form.cleaned_data.get('title'),
+                text=form.cleaned_data.get('text'),
+
+            )
+            new_task.save()
+            new_task.tags.set(form.cleaned_data.get('tags')),
+            context['form'] = AddTaskForm()
+    elif request.method == 'GET':
+        #if request.user == user:
+        form = AddTaskForm()
+        context['form'] = form
     return render(request, 'tickets/task_list.html', context)
 
 @login_required(login_url='/admin/login')
